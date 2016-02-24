1cccf94adfd704d12da9e0a2a62e7c590fbd8516
saranpong <saranpong@bangkokair.com>
2016-02-24 11:33:27 +0700

    activity edit

--- Bonobo.Git.Server/Controllers/ActivityController.cs
+++ Bonobo.Git.Server/Controllers/ActivityController.cs
@@ -7,13 +7,20 @@ using System.Web;
 using System.Web.Mvc;
 using Bonobo.Git.Server.Models;
 using LibGit2Sharp;
+using Microsoft.Practices.Unity;
+using Bonobo.Git.Server.Data;
 
 namespace Bonobo.Git.Server.Controllers
 {
     public class ActivityController : Controller
     {
+        [Dependency]
+        public ITeamRepository TeamRepository { get; set; }
 
-        [WebAuthorize(Roles = Definitions.Roles.Administrator)]
+        [Dependency]
+        public IRepositoryRepository RepositoryRepository { get; set; }
+
+        [WebAuthorize]
         public ActionResult LastedCommit(int page = 1)
         {
             int pageSize = 10;
@@ -23,9 +30,11 @@ namespace Bonobo.Git.Server.Controllers
 
             var list = new List<ActivityCommitModels>();
 
+            var repoList = this.GetIndexModel();
+
             foreach (var repoPath in allRepo)
             {
-                using (var repo = new Repository(repoPath.FullName))
+                using (var repo = new LibGit2Sharp.Repository(repoPath.FullName))
                 {
                     var filter = new CommitFilter
                     {
@@ -43,13 +52,19 @@ namespace Bonobo.Git.Server.Controllers
                         var committWhen = com.Committer.When;
                         var message = com.Message;
 
+                        if (repoList.Where(r => r.Name == repoPath.Name).FirstOrDefault() == null)
+                            continue;
+
+                        var repoGuid = repoList.Where(r => r.Name == repoPath.Name).FirstOrDefault().Id; 
+
                         var ac = new ActivityCommitModels();
                         ac.ProjectName = repoPath.Name;
                         ac.CommitterName = committer;
                         ac.Email = committMail;
                         ac.When = committWhen.UtcDateTime;
                         ac.Message = message;
-                        ac.id = id;
+                        ac.idSha = id;
+                        ac.id = repoGuid;
                         list.Add(ac);
                     }
 
@@ -65,6 +80,53 @@ namespace Bonobo.Git.Server.Controllers
             return View(list);
         }
 
+
+        private IEnumerable<RepositoryDetailModel> GetIndexModel()
+        {
+            IEnumerable<RepositoryModel> repositoryModels;
+            if (User.IsInRole(Definitions.Roles.Administrator))
+            {
+                repositoryModels = RepositoryRepository.GetAllRepositories();
+            }
+            else
+            {
+                var userTeams = TeamRepository.GetTeams(User.Id()).Select(i => i.Id).ToArray();
+                repositoryModels = RepositoryRepository.GetPermittedRepositories(User.Id(), userTeams);
+            }
+            return repositoryModels.Select(ConvertRepositoryModel).ToList();
+        }
+
+        private RepositoryDetailModel ConvertRepositoryModel(RepositoryModel model)
+        {
+            return model == null ? null : new RepositoryDetailModel
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Group = model.Group,
+                Description = model.Description,
+                Users = model.Users,
+                Administrators = model.Administrators,
+                Teams = model.Teams,
+                IsCurrentUserAdministrator = model.Administrators.Select(x => x.Username).Contains(User.Username(), StringComparer.OrdinalIgnoreCase),
+                AllowAnonymous = model.AnonymousAccess,
+                Status = GetRepositoryStatus(model),
+                AuditPushUser = model.AuditPushUser,
+                Logo = new RepositoryLogoDetailModel(model.Logo),
+            };
+        }
+
+        private RepositoryDetailStatus GetRepositoryStatus(RepositoryModel model)
+        {
+            string path = Path.Combine(UserConfiguration.Current.Repositories, model.Name);
+            if (!Directory.Exists(path))
+            {
+                return RepositoryDetailStatus.Missing;
+            }
+            else
+            {
+                return RepositoryDetailStatus.Valid;
+            }
+        }
 
     }
 }
--- Bonobo.Git.Server/Models/ActivityCommitModels.cs
+++ Bonobo.Git.Server/Models/ActivityCommitModels.cs
@@ -7,7 +7,8 @@ namespace Bonobo.Git.Server.Models
 {
     public class ActivityCommitModels
     {
-        public string id { get; set; }
+        public Guid id { get; set; }
+        public string idSha { get; set; }
         public string ProjectName { get; set; }
         public string CommitterName { get; set; }
         public string Email { get; set; }
